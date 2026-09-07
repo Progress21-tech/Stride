9046633c688f277baf0034a166e1e293b8bb0731
Progress Oni <dreaxellprobe@gmail.com>
2026-09-07 14:25:51 +0100

    Updated tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,7 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   darkMode: 'class',
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './index.html'
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -17,18 +14,7 @@ const config: Config = {
           'green-light': '#e8f7ee',
           'green-dark': '#0d5c30',
         },
-        surface: {
-          dark: '#09090b',
-          'dark-card': '#121215',
-          'dark-border': '#27272a',
-          light: '#ffffff',
-          'light-card': '#f4f4f5',
-          'light-border': '#e4e4e7',
-        }
       },
-      backdropBlur: {
-        xs: '2px',
-      }
     },
   },
   plugins: [],
